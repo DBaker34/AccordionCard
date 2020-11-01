0cf93b4ce2a152bc5e3ba35a968a2450bc1fc4a0
Dbake34 <64293380+Dbake34@users.noreply.github.com>
2020-11-01 02:32:05 -0500

    Create faq.js

---
+++ js/faq.js
@@ -0,0 +1,39 @@
+$("#head1").click(function(){
+    $("#p1").toggleClass("hidden"); 
+});
+
+$("#head2").click(function(){
+    $("#p2").toggleClass("hidden"); 
+});
+
+$("#head3").click(function(){
+    $("#p3").toggleClass("hidden"); 
+});
+
+$("#head4").click(function(){
+    $("#p4").toggleClass("hidden"); 
+});
+
+$("#head5").click(function(){
+    $("#p5").toggleClass("hidden"); 
+});
+
+$("#head1").click(function(){
+    $(this).toggleClass("bold"); 
+});
+
+$("#head2").click(function(){
+    $(this).toggleClass("bold"); 
+});
+
+$("#head3").click(function(){
+    $(this).toggleClass("bold"); 
+});
+
+$("#head4").click(function(){
+    $(this).toggleClass("bold"); 
+});
+
+$("#head5").click(function(){
+    $(this).toggleClass("bold"); 
+});
